f47b1e41e1a5358acb13a219573736f6b92c289c
Leonardo Suave <leonardo.suave@aoatechpartners.com.br>
2023-03-27 12:37:56 -0300

    feat: Post users

---
+++ src/user/user.controller.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post } from "@nestjs/common";
+
+@Controller('users')
+export class UserController {
+
+    @Post()
+    async create(@Body() body) {
+        return {};
+    }
+}
