5d346c82efc13566a18e3659d16d452b1dbdf067
Leonardo Suave <leonardo.suave@aoatechpartners.com.br>
2023-03-27 12:59:06 -0300

    feat: example crud on nestJS

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,44 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
 
 @Controller('users')
 export class UserController {
 
     @Post()
     async create(@Body() body) {
-        return {};
+        return {body};
+    }
+
+    @Get()
+    async read() {
+        return { user: [] }
+    }
+
+    @Get(':id')
+    async readOne(@Param() param) {
+        return { user: {}, param }
+    }
+
+    @Patch(':id')
+    async updatePartial(@Body() body, @Param() param) {
+        return {
+            method: 'put',
+            body,
+            param
+        };
+    }
+
+    @Put(':id')
+    async update(@Body() body, @Param() param) {
+        return {
+            method: 'put',
+            body,
+            param
+        };
+    }
+
+    @Delete(':id')
+    async delete(@Param() param) {
+        return { param }
     }
 }
