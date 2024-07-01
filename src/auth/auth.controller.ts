ae32b7a2ad89d74bd98a0dadc6aa5c29b47fe453
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2024-07-01 13:41:19 -0300

    feat: create guards to verify user token and return user infos and another route and guards to verify if token in use is only valid (#9)
    Co-authored-by: Leonardo Suave <leonardo.suave@aoatechpartners.com.br>

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { AuthLoginDTO } from './dto/auth-login.dto';
 import { AuthRegisterDTO } from './dto/auth-register.dto';
 import { ForgetPasswordDTO } from './dto/forget-password.dto';
 import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 import { ResetPasswordDTO } from './dto/reset-password.dto';
+import { AuthGuard } from 'src/guards/auth-guard';
+import { AuthStatusGuard } from 'src/guards/auth-status-guard';
 
 @Controller('auth')
 export class AuthController {
@@ -33,5 +42,17 @@ export class AuthController {
   async resetPassword(@Body() data: ResetPasswordDTO) {
     await this.authService.resetPassword(data);
     return;
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('me')
+  async me(@Request() request) {
+    return request.userByToken;
+  }
+
+  @UseGuards(AuthStatusGuard)
+  @Get('me/status')
+  async meStatus(@Request() request) {
+    return { status: request.userStatus };
   }
 }
---
+++ src/guards/auth-guard.ts
@@ -0,0 +1,22 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly authService: AuthService) {}
+
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const { authorization } = request.headers;
+    try {
+      if (authorization && authorization.startsWith('Bearer')) {
+        const accessToken = authorization.split(' ')[1];
+        const user = this.authService.checkToken(accessToken);
+        request.userByToken = user;
+        return true;
+      }
+    } catch (error) {
+      return false;
+    }
+  }
+}
