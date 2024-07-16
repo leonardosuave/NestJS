87f7cffd1e4c5d54057a9a7d22636df48a97fbb1
Leonardo Suave <leonardo.suave@aoatechpartners.com.br>
2024-07-16 17:27:53 -0300

    feat: add throttler to DDOS attack

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { LogInterceptor } from './interceptors/log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LogInterceptor());
   await app.listen(3000);
