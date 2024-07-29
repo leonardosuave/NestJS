34064284308bc2fdf94da225ccbaf237190ca334
Leonardo Suave <leonardo.suave@aoatechpartners.com.br>
2024-07-29 14:43:54 -0300

    feat: add nodemailer to send emails and recovery and reset password users

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { AuthModule } from './auth/auth.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
   imports: [
@@ -16,6 +18,26 @@ import { ConfigModule } from '@nestjs/config';
     }),
     forwardRef(() => UserModule),
     forwardRef(() => AuthModule),
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+          user: 'myles.leannon@ethereal.email',
+          pass: 'bZnYPbSGgZdyYRUMnw',
+        },
+      },
+      defaults: {
+        from: '"nest-study" <myles.leannon@ethereal.email>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [
